dde7025b80b6703be4df8a56b24a0e8505984fbd
Florian Kugler <mail@floriankugler.com>
2015-01-01 17:08:23 +0100

    Fix up some redirects

--- config.rb
+++ config.rb
@@ -73,24 +73,49 @@ configure :build do
 end
 
 
+
+# Redirects for old Squarespace blog
+
+# S3 Routing rules to redirect the RSS feed:
+# <RoutingRules>
+#     <RoutingRule>
+#         <Condition>
+#             <KeyPrefixEquals>blog?format=rss</KeyPrefixEquals>
+#         </Condition>
+#         <Redirect>
+#             <ReplaceKeyPrefixWith>feed.xml</ReplaceKeyPrefixWith>
+#         </Redirect>
+#     </RoutingRule>
+#     <RoutingRule>
+#         <Condition>
+#             <KeyPrefixEquals>blog?format=RSS</KeyPrefixEquals>
+#         </Condition>
+#         <Redirect>
+#             <ReplaceKeyPrefixWith>feed.xml</ReplaceKeyPrefixWith>
+#         </Redirect>
+#     </RoutingRule>
+# </RoutingRules>
+
 redirect '/blog', '/'
 redirect '/talks', '/projects'
-redirect '/blog/2013/4/2/the-lack-of-self-knowledge-in-tv-movies', '/2013/03/24/the-lack-of-self-knowledge-in-tv-movies/'
+redirect '/blog/2013/4/2/how-to-not-make-an-argument', '/'
+redirect '/blog/2013/4/2/why-users-have-no-power', '/'
+redirect '/blog/2013/4/2/the-lack-of-self-knowledge-in-tv-movies', '/'
 redirect '/blog/2013/4/2/making-autolayout-code-less-painful', '/2013/03/26/making-autolayout-code-less-painful/'
 redirect '/blog/2013/4/2/the-concurrent-core-data-stack', '/2013/04/02/the-concurrent-core-data-stack/'
 redirect '/blog/2013/4/4/the-economics-of-app-store-pricing', '/2013/04/05/the-economics-of-app-store-pricing/'
 redirect '/blog/2013/4/8/a-self-experiment-with-nutritional-ketosis', '/2013/04/08/a-self-experiment-with-nutritional-ketosis/'
 redirect '/blog/2013/4/15/interface-builder-ndash-curse-or-convenience', '/2013/04/15/interface-builder-ndash-curse-or-convenience/'
-redirect '/blog/2013/4/15/interface-builder-ndash-curse-or-convenience', '/2013/04/22/auto-layout-performance-on-ios/'
+redirect '/blog/2013/4/21/auto-layout-performance-on-ios', '/2013/04/22/auto-layout-performance-on-ios/'
 redirect '/blog/2013/4/29/concurrent-core-data-stack-performance-shootout', '/2013/04/29/concurrent-core-data-stack-performance-shootout/'
-redirect '/blog/2013/4/29/concurrent-core-data-stack-performance-shootout', '/2013/05/13/backstage-with-nested-managed-object-contexts/'
+redirect '/blog/2013/5/11/backstage-with-nested-managed-object-contexts', '/2013/05/13/backstage-with-nested-managed-object-contexts/'
 redirect '/blog/2013/5/24/layer-trees-vs-flat-drawing-graphics-performance-across-ios-device-generations', '/2013/05/24/layer-trees-vs-flat-drawing-graphics-performance-across-ios-device-generations/'
-redirect '/blog/2013/5/24/layer-trees-vs-flat-drawing-graphics-performance-across-ios-device-generations', '/2013/06/05/uikonf-presentation-app-optimization-with-instruments/'
-redirect '/blog/2013/6/5/uikonf-presentation-app-optimization-with-instruments', '/2013/06/07/announcing-objcio/'
+redirect '/blog/2013/6/5/uikonf-presentation-app-optimization-with-instruments', '/2013/06/05/uikonf-presentation-app-optimization-with-instruments/'
+redirect '/blog/2013/6/7/announcing-objcio', '/2013/06/07/announcing-objcio/'
 redirect '/blog/2013/7/6/objcio-2-concurrent-programming', '/2013/07/08/objcio-2-concurrent-programming/'
 redirect '/blog/2013/8/4/upcoming', '/2013/08/04/upcoming/'
 redirect '/blog/2013/9/30/worth-less-than-a-cup-of-coffee', '/2013/09/30/worth-less-than-a-cup-of-coffee/'
 redirect '/blog/2013/12/20/building-a-mac-app-deckset', '/2013/12/27/building-a-mac-app-deckset/'
-redirect '/blog/2013/12/20/building-a-mac-app-deckset', '/2014/02/18/alt-tech-talks-london-one-cannot-not-communicate/'
-redirect '/blog/2014/2/18/alt-tech-talks-london-one-cannot-not-communicate', '/2014/04/07/deckset-has-launched/'
+redirect '/blog/2014/2/18/alt-tech-talks-london-one-cannot-not-communicate', '/2014/02/18/alt-tech-talks-london-one-cannot-not-communicate/'
+redirect '/blog/2014/4/6/deckset-has-launched', '/2014/04/07/deckset-has-launched/'
 redirect '/blog/2014/9/29/functional-programming-in-swift', '/2014/10/03/functional-programming-in-swift/'
